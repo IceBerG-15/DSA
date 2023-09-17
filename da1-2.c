615ff8eb912324a6a56d15bed93a8ab190fd935b
Sidhant Sharma <warmachiness13@gmail.com>
2023-09-17 22:42:34 +0530

    Add files via upload

--- da1-2.c
+++ da1-2.c
@@ -1,70 +1,90 @@
-//queue using stack 
 #include <stdio.h>
 #include <stdlib.h>
 
-#define SIZE 10
+typedef struct stack {
+    int data;
+    struct stack* next;
+} Stack;
 
-int stack[SIZE];
-int top1=-1;
+Stack* createNode(int data) {
+    Stack* newNode = (Stack*)malloc(sizeof(Stack));
+    if (!newNode) {
+        fprintf(stderr, "Memory allocation failed\n");
+        exit(1);
+    }
+    newNode->data = data;
+    newNode->next = NULL;
+    return newNode;
+}
 
-void push();
-void queue_display();
-void stack_display();
+void push(Stack** top, int data) {
+    Stack* newNode = createNode(data);
+    newNode->next = *top;
+    *top = newNode;
+}
 
-int main()
-{
-    int ch;
-    while(ch<4)
-    {
-        printf("\n MENU: ");
-        printf(" \n 1-PUSH \n 2-STACK-DISPLAY \n 3-QUEUE-DISPLAY \n 4-EXIT");
-        printf("\n enter your choice: ");
-        scanf("%d",&ch);
-        switch(ch)
-        {
-            case 1:
-                push();
-                break;
-            case 2:
-                stack_display();
-                break;
-            case 3:
-                queue_display();
-                break;
-            case 4:
-                printf("exitted");
-                break;
-            default:
-                printf("invalid choice");
-                break;
+int pop(Stack** top) {
+    if (*top == NULL) {
+        fprintf(stderr, "Stack underflow\n");
+        exit(1);
+    }
+    Stack* temp = *top;
+    int poppedData = temp->data;
+    *top = (*top)->next;
+    free(temp);
+    return poppedData;
+}
+
+void enqueue(Stack** queue, int data) {
+    push(queue, data);
+}
+
+int dequeue(Stack** inputStack, Stack** outputStack) {
+    if (*outputStack == NULL) {
+        while (*inputStack != NULL) {
+            push(outputStack, pop(inputStack));
         }
     }
+    return pop(outputStack);
+}
+
+void displayQueue(Stack* inputStack, Stack* outputStack) {
+    printf("Queue contents: ");
+    while (inputStack != NULL) {
+        push(&outputStack, pop(&inputStack));
+    }
+    while (outputStack != NULL) {
+        printf("%d", pop(&outputStack));
+        
+        if (outputStack != NULL) {
+            printf(", ");
+        }
+    }
+    
+    printf("\n");
+}
+
+
+int main() {
+    Stack* inputStack = NULL;
+    Stack* outputStack = NULL;
+    
+    int n = 0, i = 0, data;
+    while(n !=3){
+        printf("-------MENU--------\n1. Enqueue\n2.Display\n3.Exit\n--------------");
+        printf("\nEnter your choice:");
+        scanf("%d", &n);
+        if(n == 1){
+            printf("Enter the number you want to add in Queue : \t");
+            scanf("%d", &data);
+            enqueue(&inputStack,data);
+        }
+        if(n == 2){
+            displayQueue(inputStack, outputStack);
+        }
+    }
+
+    displayQueue(inputStack, outputStack);
+
     return 0;
-}
-
-void push()
-{   
-    int x;
-    printf("enter number: ");
-    scanf("%d",&x);
-    stack[++top1] = x;
-}
-
-
-void queue_display()
-{
-    int top2=-1;
-    int queue[SIZE];
-    for(int i=top1; i>-1; i--)
-        queue[++top2] = stack[i];
-    printf("\n QUEUE: ");
-    for(int i=top2; i>-1; i--)
-        printf(" %d ",queue[i]);
-}
-
-void stack_display()
-{
-    printf("\n STACK: ");
-    for(int i=top1; i>-1; i--)
-        printf(" %d ",stack[i]);
 }
