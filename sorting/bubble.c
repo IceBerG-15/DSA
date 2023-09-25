83f7406d993d394fc5b51fcb0d2b6ffe99b9205e
Sidhant Sharma <warmachiness13@gmail.com>
2023-09-25 22:45:58 +0530

    Update bubble.c

--- sorting/bubble.c
+++ sorting/bubble.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void bubble_sort(int a[],int n);
+void bubble_sort(int a[],int n);//function initialization
 
 int main()
 {
@@ -14,13 +14,13 @@ int main()
         printf("\n enter element: ");
         scanf("%d",&a[i]);
     }
-    bubble_sort(a,n);
+    bubble_sort(a,n);//function call
 }
 
-void bubble_sort(int a[],int n)
+void bubble_sort(int a[],int n)//function definition
 {
     int temp;
-    int s;
+    int s;//swap variable
     for(int i=0;i<n;i++)
     {   
         s=0;
@@ -31,13 +31,13 @@ void bubble_sort(int a[],int n)
                 temp=a[j];
                 a[j]=a[j+1];
                 a[j+1]=temp;
-                s=1;
+                s=1;//if swapping is done then it value is 1
             }
         }
-        if(s==0)
+        if(s==0)//if no swapping is done in the j-loop then it means array is already sorted, so break out of loop.
             break;
         printf("\nelement after %d pass.",i+1);
-        for(int k=0;k<n;k++)
+        for(int k=0;k<n;k++)//to show array after every pass.
             printf("%d ",a[k]);
         
     }
